14609e29f751fd6ecadd508dab12c76362113567
Loki Holbourn <loki.holbourn@nationalarchives.gov.uk>
2020-01-28 16:06:58 +0000

    refactored original code calling API & dealing with results. Now fixing view to reflect api results, will use bootstrap cards & grid to create a view which is easy to read & functional

--- app/controllers/ApiController.scala
+++ app/controllers/ApiController.scala
@@ -6,18 +6,14 @@ import io.circe.generic.auto._
 import io.circe.parser._
 import io.circe.Decoder
 import javax.inject.Inject
-import models.{ApiResult, WeatherDesc, WeatherInfo}
+import models.{ApiResult, Forecast, WeatherDesc, WeatherInfo}
 import play.api.mvc.{Request, _}
 import cats.syntax.either._
 
 
-
-
-
-
 class ApiController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
 
-  def apiCall: Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+  def apiCall: Action[AnyContent] =  Action { implicit request: Request[AnyContent] =>
     val apiKey = sys.env("WEATHER_API_KEY")
     val request = sttp.get(
       uri"https://api.openweathermap.org/data/2.5/forecast?lat=51.482056&lon=-0.280056&APPID=${apiKey}"
@@ -27,67 +23,72 @@ class ApiController @Inject()(val controllerComponents: ControllerComponents) ex
     val responseBody: String = response.unsafeBody
     val decodedBody: Either[Error, ApiResult] = decode[ApiResult](responseBody)
 
+    def testApiResults(apiResult: ApiResult): Array[Forecast] = {
+      val testApiResult: ApiResult = decodedBody match {
+        case Right(result: ApiResult) => result
+      }
+      apiResultWithMaps(testApiResult)
+    }
 
-
-    val result: Result = decodedBody match {
-      case Right(result: ApiResult) => Ok(views.html.api(result))
+    decodedBody match {
+      case Right(result) => Ok(views.html.api(testApiResults(result)))
       case Left(error: Error) =>
         println(error)
         InternalServerError(views.html.error("Not working"))
     }
-
-    val testApiResult: ApiResult = decodedBody match {
-      case Right(result: ApiResult) => result
-    }
-    val whatWeGot: Unit = apiResults(testApiResult)
-
-    result
   }
 
 
-
-  def apiResults(apiResult: ApiResult) = {
-
-    for (el: Json <- apiResult.list) {
-      var index: Int = 0
-
-      println("\n******************")
-
-      val decodedWeather: Either[Error, WeatherInfo] = decode[WeatherInfo](el.toString())
+  def apiResultWithMaps(apiResult: ApiResult): Array[Forecast] = {
+    //   MAP RATHER THAN FOR LOOP
+    apiResult.list.flatMap { jsonInstance =>
+    val decodedWeather: Either[Error, WeatherInfo] = decode[WeatherInfo](jsonInstance.toString())
       val weatherInfo: WeatherInfo = decodedWeather match {
-        case Right(result: WeatherInfo) =>  result
-
+        case Right(result: WeatherInfo) => result
       }
-
       val weather: Array[Json] = weatherInfo.weather
-      for (item <- weather) {
-        val decodedWeatherInfo: Either[Error, WeatherDesc] = decode[WeatherDesc](item.toString)
+      weather.map { info =>
+      val decodedWeatherInfo: Either[Error, WeatherDesc] = decode[WeatherDesc](info.toString)
         val weatherDesc: WeatherDesc = decodedWeatherInfo match {
           case Right(desc: WeatherDesc) => desc
         }
-        println(s"weather desc = ${weatherDesc.description}")
+        Forecast(dateAndTime = weatherInfo, description = weatherDesc)
       }
-
-      print(s"weather date = ${weatherInfo.dt_txt}")
-      index += 1
     }
-
   }
-
-//  case class Forecast(dateAndTime: String, descritoin: String)
-//  def apiResultWithMaps(apiResult: ApiResult): Seq[Forecast] = {
-  // MAP RATHER THAN FOR LOOP
-  //
-  //  }
-
-
-
-
 }
 
+//  Go into JSON list (apiResult.list)
+//  For every JSON instance in list =
+//      -> Create WeatherInfo instance (dt_txt & weather[Json Array])
+//        -> Go into WeatherInfo instance & create instance of WeatherDesc from weatherInfo (weatherDesc.description)
+//          -> Save these instances to a seq of Forecasts (with dateAndTime & description)
+//  Feed these to view to show to user!!!!!!
 
 
+//  Original method of getting WeatherInfo & WeatherDesc
+//  Does not create instances of case class Forecast
 
-//      val cursor: HCursor = el.hcursor
-//      val dt: Decoder.Result[Int] = cursor.downField("dt").as[Int]
-//      val weather: Decoder.Result[String] = cursor.downField("weather").downN(2).as[String]
+//  def apiResults(apiResult: ApiResult) = {
+//
+//    for (el: Json <- apiResult.list) {
+//      var index: Int = 0
+//      println("\n******************")
+//      val decodedWeather: Either[Error, WeatherInfo] = decode[WeatherInfo](el.toString())
+//      val weatherInfo: WeatherInfo = decodedWeather match {
+//        case Right(result: WeatherInfo) =>  result
+//      }
+//
+//      val weather: Array[Json] = weatherInfo.weather
+//      for (item <- weather) {
+//        val decodedWeatherInfo: Either[Error, WeatherDesc] = decode[WeatherDesc](item.toString)
+//        val weatherDesc: WeatherDesc = decodedWeatherInfo match {
+//          case Right(desc: WeatherDesc) => desc
+//        }
+//        println(s"weather desc = ${weatherDesc.description}")
+//      }
+//
+//      print(s"weather date = ${weatherInfo.dt_txt}")
+//      index += 1
+//    }
+//  }
